524df06b392b8d2803e02d62e5987dcbf08a28d3
darinisawesome <github@darinisawesome.com>
2017-07-13 12:07:52 -0700

    Added the dplyr library (I thought this was included in tidyverse, but %>% wasn't working)
    Added helpful comments
    
    Added lines to manually rename company names
    
    Sorted alphabetically by company name

--- Refine.R
+++ Refine.R
@@ -1,6 +1,17 @@
+#load necessary libraries
 library(tidyverse)
+library(dplyr)
 library(readr)
 
+#load original dataset to be cleaned
 refine_original <- read_csv("~/Github/Springboard_Foundations_Data-Refine_01/refine_original.csv")
 View(refine_original)
 
+#change row name data manually by specific row
+refine_original[1:6,1] <- "philips"
+refine_original[7:13,1] <- "akzo"
+refine_original[14:16,1] <- "philips"
+refine_original[22:25,1] <- "unilever"
+
+#sort alphabetically by company name
+refine_original <- refine_original %>% arrange(company)
