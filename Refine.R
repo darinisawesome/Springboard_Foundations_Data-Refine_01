9e5c329746f76ab1589e85f792bd29616881ca6d
darinisawesome <github@darinisawesome.com>
2017-07-14 07:37:42 -0700

    Added the tidyr library
    Added separate to split 'Product code / number' into "product_code" and "product_number" where "-" separated the two columns
    
    Added columns for device type (Smartphone, TV, Laptop, Tablet), and added logical data related to row device type
    
    Changed spacing to make %>%s easier to read

--- Refine.R
+++ Refine.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(dplyr)
 library(readr)
+library(tidyr)
 
 #load original dataset to be cleaned
 refine_original <- read_csv("~/Github/Springboard_Foundations_Data-Refine_01/refine_original.csv")
@@ -13,5 +14,18 @@ refine_original[7:13,1] <- "akzo"
 refine_original[14:16,1] <- "philips"
 refine_original[22:25,1] <- "unilever"
 
-#sort alphabetically by company name
-refine_original <- refine_original %>% arrange(company)
+#sort alphabetically by company nameproduct
+refine_original <- refine_original %>% 
+  arrange(company)
+
+#separate "Product code / number" column into product_code and product number and remove it aftewards
+refine_original <- refine_original %>% 
+  separate('Product code / number', c("product_code","product_number"),sep = "-", remove=TRUE)
+
+#add logical operator for what type of device each row contains
+refine_original <- refine_original %>% 
+  mutate(Smartphone = product_code == 'p') %>% 
+  mutate(TV = product_code == 'v') %>% 
+  mutate(Laptop = product_code == 'x') %>% 
+  mutate(Tablet = product_code == 'q')
+
